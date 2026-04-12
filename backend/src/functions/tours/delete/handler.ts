90c0966363e0d390bc0098a5db1666dcac7911b4
wooglie <imagas96@gmail.com>
2026-04-12 19:50:28 +0200

    chore: trigger deploy on tour delete

--- backend/src/functions/tours/delete/handler.ts
+++ backend/src/functions/tours/delete/handler.ts
@@ -1,9 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { getItem, deleteItem } from "../../../lib/dynamodb";
-import { noContent, badRequest, notFound, serverError } from "../../../lib/response";
+import {
+  noContent,
+  badRequest,
+  notFound,
+  serverError,
+} from "../../../lib/response";
+import { triggerTourAppDeploy } from "../../../lib/triggerDeploy";
 
 export const handler = async (
-  event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
     const id = event.pathParameters?.id;
@@ -18,6 +24,8 @@ export const handler = async (
     }
 
     await deleteItem({ TableName: table, Key: { id } });
+
+    await triggerTourAppDeploy();
 
     return noContent();
   } catch (err) {
